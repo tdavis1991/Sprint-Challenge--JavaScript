dd43d398389d9da717187ae6e6e1c6c5ea87e770
tevin davis <aod.td1105@gmail.com>
2019-07-14 19:30:19 -0400

    Finished the challenges for classes.js file

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -5,11 +5,11 @@
 /* == Step 1: Base Constructor ==
   Create a constructor function named CuboidMaker that accepts properties for length, width, and height
 */
-function CuboidMaker(cube) {
-  this.length = cube.length;
-  this.width = cube.width;
-  this.height = cube.height;
-};
+// function CuboidMaker(cube) {
+//   this.length = cube.length;
+//   this.width = cube.width;
+//   this.height = cube.height;
+// };
 
 
 /* == Step 2: Volume Method ==
@@ -17,9 +17,9 @@ function CuboidMaker(cube) {
   
   Formula for cuboid volume: length * width * height
 */
-CuboidMaker.prototype.volume = function() {
-  return this.length * this.width * this.height
-};
+// CuboidMaker.prototype.volume = function() {
+//   return this.length * this.width * this.height
+// };
 
 
 
@@ -28,23 +28,23 @@ CuboidMaker.prototype.volume = function() {
 
   Formula for cuboid surface area of a cube: 2 * (length * width + length * height + width * height)
 */
-CuboidMaker.prototype.surfaceArea = function() {
-  return 2 * (this.length * this.width + this.length * this.height + this.width * this.height)
-};
+// CuboidMaker.prototype.surfaceArea = function() {
+//   return 2 * (this.length * this.width + this.length * this.height + this.width * this.height)
+// };
 
 
 /* == Step 4: Create a new object that uses CuboidMaker ==
   Create a cuboid object that uses the new keyword to use our CuboidMaker constructor
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
 */
-const cuboid = new CuboidMaker ({
-  length: 4,
-  width: 5,
-  height: 5
-});
+// const cuboid = new CuboidMaker ({
+//   length: 4,
+//   width: 5,
+//   height: 5
+// });
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-console.log(cuboid.volume()); // 100
-console.log(cuboid.surfaceArea()); // 130
+// console.log(cuboid.volume()); // 100
+// console.log(cuboid.surfaceArea()); // 130
 
 
